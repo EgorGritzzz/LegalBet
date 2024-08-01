913f2c0bc098141ce774407c61e767da593a31d4
EgorGritzzz <134557979+EgorGritzzz@users.noreply.github.com>
2024-08-01 18:10:49 +0300

    Update main.js

--- vite-project/src/js/main.js
+++ vite-project/src/js/main.js
@@ -1,13 +1,13 @@
 let toggleButton = document.querySelector('.toggle__button');
 let toggleText = document.querySelector('.hidden');
-let rotatedArrow  = document.querySelector('#arrow')
+let rotatedArrow  = document.querySelector('#arrow');
 let readText = document.querySelector('.read_next');
 
 toggleButton.addEventListener('click',()=>{
   if (toggleText.style.display === 'none') {
     toggleText.style.display = 'block';
     rotatedArrow.style.transform  = 'rotate(180deg)';
-    rotatedArrow.style.transition = ' 0.5s ease'
+    rotatedArrow.style.transition = ' 0.5s ease';
     readText.innerText = '';
   } else {
     toggleText.style.display = 'none';
